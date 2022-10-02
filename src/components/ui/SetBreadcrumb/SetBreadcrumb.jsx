63fa1dd674f7e1c9b828d7113eb52529aaac5e90
Maria Holm <maria.lilleberg@gmail.com>
2022-10-02 14:15:07 +0200

    link fix

--- src/components/ui/SetBreadcrumb/SetBreadcrumb.jsx
+++ src/components/ui/SetBreadcrumb/SetBreadcrumb.jsx
@@ -1,18 +1,17 @@
 import Breadcrumb from "react-bootstrap/Breadcrumb";
 import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
 
 function SetBreadcrumb({ name }) {
   return (
     <Breadcrumb>
-      <Breadcrumb.Item className="breadcrumb__item" href="/">
-        Home
-      </Breadcrumb.Item>
-      <Breadcrumb.Item
-        className="breadcrumb__item breadcrumb__item--estab"
-        href="/establishments"
-      >
-        Establishments
-      </Breadcrumb.Item>
+      <li className="breadcrumb-item breadcrumb__item">
+        <Link to="/">Home</Link>
+      </li>
+      <li className="breadcrumb-item breadcrumb__item breadcrumb__item--estab">
+        {" "}
+        <Link to="/establishments">Establishments</Link>
+      </li>
       <Breadcrumb.Item
         className="breadcrumb__item breadcrumb__item--active"
         active
